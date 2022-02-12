8c11624378f2efa2523242d407f20f39c16e7dfa
antortjim <ntoniohu@gmail.com>
2022-02-12 01:10:26 +0000

    Upload finish. need to implement uploads > 150 MB

--- flyhostel/data/upload.py
+++ flyhostel/data/upload.py
@@ -17,30 +17,33 @@ def get_parser(ap=None):
 
 def upload_chunk(experiment_folder, chunk):
 
-    files = sorted(glob.glob(os.path.join(experiment_folder, f"{str(chunk).zfill(6)}*")))
-    files.append(f"session_{str(chunk).zfill(6)}-local_settings.py")
+    files = sorted(glob.glob(os.path.join(experiment_folder.rstrip("/"), f"{str(chunk).zfill(6)}*")), reverse=True)
+    files.append(os.path.join(experiment_folder.rstrip("/"), f"session_{str(chunk).zfill(6)}-local_settings.py"))
     experiment_name = os.path.basename(experiment_folder.rstrip("/"))
 
     for fullname in tqdm.tqdm(files):
-        fullname = fullname.replace(experiment_name, f"{experiment_name}/./")
+        fullname = fullname.replace(experiment_name, f"{experiment_name}/.")
         name = os.path.basename(fullname)
-        p = subprocess.Popen([
+        cmd = [
             "dropy",
             fullname,
-            f"Dropbox:/Data/flyhostel_data/videos/{experiment_name}"
-        ])
-
+            f"Dropbox:/Data/flyhostel_data/videos/{experiment_name}/"
+        ]
+        print(cmd)
+        p = subprocess.Popen(cmd)
         p.communicate()
 
-    
+
     folder = os.path.join(experiment_folder, ".", "idtrackerai", f"session_{str(chunk).zfill(6)}")
-    subprocess.Popen([
+    cmd = [
         "dropy",
         folder,
-        f"Dropbox:/Data/flyhostel_data/videos/{experiment_name}"
-    ]).communicate()
-    
-    
+        f"Dropbox:/Data/flyhostel_data/videos/{experiment_name}/"
+    ]
+
+    print(cmd)
+    subprocess.Popen(cmd).communicate()
+
 
 def upload_chunks(experiment_folder, interval=None, chunks=None, jobs=5):
 
