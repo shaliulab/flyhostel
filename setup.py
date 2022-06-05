98e0c18d453c65b1f729bb948d9eacbc9cea654f
Antonio Ortega <ntoniohu@gmail.com>
2022-06-05 15:51:12 +0100

    Update to 1.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 PKG_NAME = "flyhostel"
-version = "1.1.0"
+version = "1.1.1"
 
 with open(f"{PKG_NAME}/__init__.py", "w") as fh:
     fh.write(f"__version__ = '{version}'\n")
