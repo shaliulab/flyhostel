61c9006b7a62551e043e4f2e74ecb0b88a8d569f
antortjim <ntoniohu@gmail.com>
2022-08-19 14:17:50 +0000

    Update __init__

--- flyhostel/__init__.py
+++ flyhostel/__init__.py
@@ -1,15 +1,25 @@
 __version__ = '1.1.3'
 
-import os
 import sys
-sys.path.insert(0, os.getcwd())
+import os
+import logging.config
+
+import yaml
 from confapp import conf
 
+from .constants import LOGGING_CONFIG
+
+sys.path.insert(0, os.getcwd())
+
 try:
-    import local_settings # type: ignore
+    import local_settings
     conf += local_settings
 except:
     pass
 
-import flyhostel.constants
-conf += flyhostel.constants
+conf += "flyhostel.constants"
+
+with open(LOGGING_CONFIG, "r") as filehandle:
+    config = yaml.load(filehandle, yaml.SafeLoader)
+
+logging.config.dictConfig(config)
