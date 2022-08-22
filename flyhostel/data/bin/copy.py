3a22c02e5b6329feaeb1f01a46f66c7cde7302cc
antortjim <ntoniohu@gmail.com>
2022-08-22 08:11:59 +0000

    Add default arguments in the fg copy entrypoint

--- flyhostel/data/bin/copy.py
+++ flyhostel/data/bin/copy.py
@@ -1,19 +1,21 @@
 import argparse
+import os.path
 from flyhostel.data.idtrackerai import copy_idtrackerai_data
+from flyhostel.constants import ANALYSIS_FOLDER
 
 def get_parser(ap=None):
     if ap is None:
         ap = argparse.ArgumentParser()
 
     ap.add_argument(
-        "--imgstore-folder", dest="imgstore_folder", required=True, type=str
+        "--imgstore-folder", dest="imgstore_folder", type=str, default=os.getcwd()
     )
 
     ap.add_argument(
-        "--analysis-folder", dest="analysis_folder", default=None, type=str
+        "--analysis-folder", dest="analysis_folder", type=str, default=os.path.join(os.getcwd(), ANALYSIS_FOLDER)
     )
     ap.add_argument(
-        "--overwrite", action="store_true", default=True,
+        "--overwrite", action="store_true", default=False,
         help="Makes a new copy of the idtrackerai's trajectory file"
         " even if a copy already exists, overwriting it"
     )
