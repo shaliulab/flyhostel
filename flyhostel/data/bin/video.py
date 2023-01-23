207425596a6314c3fa0c62d18c2f63a82e209ff7
antortjim <ntoniohu@gmail.com>
2023-01-23 20:22:59 +0000

    Update video entrypoint

--- flyhostel/data/bin/video.py
+++ flyhostel/data/bin/video.py
@@ -7,6 +7,7 @@ def get_parser():
     ap = argparse.ArgumentParser()
     ap.add_argument("--dataset", required=True, type=str, help="path to FlyHostel.db")
     ap.add_argument("--frame-number", default=None, type=int, nargs="+", help="If not passed, all frames will be used")
+    ap.add_argument("--n-jobs", type=int, default=-2, help="Number of parallel processes to make videos")
     ap.add_argument("--width", type=int, default=200)
     ap.add_argument("--height", type=int, default=200)
     ap.add_argument("--basedir", type=str, required=True, help="Folder where the output will be generated")
@@ -19,4 +20,7 @@ def main(args=None, ap=None):
         args = ap.parse_args()
 
     video_maker=SingleVideoMaker(flyhostel_dataset=args.dataset, value=args.frame_number)
-    video_maker.make_single_video(basedir=args.basedir, frameSize=(args.width, args.height))
+    if args.n_jobs == 1:
+        video_maker.make_single_video_single_process(basedir=args.basedir, frameSize=(args.width, args.height))
+    else:
+        video_maker.make_single_video_multi_process(n_jobs=args.n_jobs, basedir=args.basedir, frameSize=(args.width, args.height))
