dcc6e1298bd2135f9b7a2e46463cf4a6bf5ef4b9
antortjim <ntoniohu@gmail.com>
2023-01-23 20:56:31 +0100

    Update video entrypoint

--- flyhostel/data/bin/video.py
+++ flyhostel/data/bin/video.py
@@ -7,6 +7,9 @@ def get_parser():
     ap = argparse.ArgumentParser()
     ap.add_argument("--dataset", required=True, type=str, help="path to FlyHostel.db")
     ap.add_argument("--frame-number", default=None, type=int, nargs="+")
+    ap.add_argument("--width", type=int, default=200)
+    ap.add_argument("--height", type=int, default=200)
+    ap.add_argument("--basedir", type=str, required=True)
     return ap
 
 
@@ -16,4 +19,4 @@ def main(args=None, ap=None):
         args = ap.parse_args()
 
     video_maker=SingleVideoMaker(flyhostel_dataset=args.dataset, value=args.frame_number)
-    video_maker.make_single_video()
+    video_maker.make_single_video(basedir=args.basedir, frameSize=(args.width, args.height))
