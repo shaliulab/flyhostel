396ddef419b49f4942b942e3c1cec3590a56bb0c
antortjim <ntoniohu@gmail.com>
2023-01-23 20:12:48 +0100

    Update video.py

--- flyhostel/data/video.py
+++ flyhostel/data/video.py
@@ -33,15 +33,16 @@ class SingleVideoMaker:
             cmd = 'SELECT value FROM METADATA WHERE field = "idtrackerai_conf";'
             cur.execute(cmd)
             conf = cur.fetchone()[0]
-            self._number_of_animals = int(json.loads(conf[0])["_number_of_animals"]["value"])
+            #import ipdb; ipdb.set_trace()
+            self._number_of_animals = int(json.loads(conf)["_number_of_animals"]["value"])
             
             cmd = 'SELECT value FROM METADATA WHERE field = "framerate";'
             cur.execute(cmd)
-            self.framerate=int(cur.fetchone()[0])
+            self.framerate=int(float(cur.fetchone()[0]))
 
             cmd = 'SELECT value FROM METADATA WHERE field = "chunksize";'
             cur.execute(cmd)
-            self.chunksize=int(cur.fetchone()[0])
+            self.chunksize=int(float(cur.fetchone()[0]))
 
 
         if value is None:
@@ -52,8 +53,6 @@ class SingleVideoMaker:
             assert value[1] <= self.max_frame_number
             self._value = value
 
-        self._value = value
-        
         self._video_object_list={}
         self.video_writer = None
 
@@ -68,7 +67,7 @@ class SingleVideoMaker:
         
         return angle
 
-    def init_video_writer(self, frameSize):
+    def init_video_writer(self, basedir, frameSize):
 
         # self.video_writer = cv2cuda.VideoWriter(
         #     os.path.join(folder, os.path.splitext(os.path.basename(self._flyhostel_dataset))[0], +".mp4"),
@@ -78,8 +77,6 @@ class SingleVideoMaker:
         #     frameSize=frameSize,
         #     isColor=False,
         # )
-        basedir = os.path.join(self._basedir, "flyhostel", "single_animal")
-
         self.video_writer = imgstore.new_for_format(
             mode="w",
             fmt=ENCODER_FORMAT_GPU,
@@ -95,27 +92,28 @@ class SingleVideoMaker:
         with sqlite3.connect(self._index, check_same_thread=False) as conn:
 
             cur = conn.cursor()
-            cmd = "SELECT chunk FROM frames WHERE frame_number = (?);"
-            cur.execute(cmd, frame_number)
+            cmd = f"SELECT chunk FROM frames WHERE frame_number = {frame_number};"
+            cur.execute(cmd)
             chunk = cur.fetchone()[0]
             return chunk
 
 
-    def make_single_video(self, frameSize):
+    def make_single_video(self, basedir, frameSize):
 
         width, height = frameSize
 
         with sqlite3.connect(self._index, check_same_thread=False) as conn:
 
             cur = conn.cursor()
-            for chunk in range(self._value[0], self._value[1]+1):
+            chunks=range(self.frame_number2chunk(self._value[0]), self.frame_number2chunk(self._value[1])+1)
+            for chunk in chunks:
                 episode_images = sorted(glob.glob(f"idtrackerai/session_{str(chunk).zfill(6)}/segmentation_data/episode_images*"), key=lambda f: int(os.path.splitext(f)[0].split("_")[-1]))
                 for episode_image in tqdm(episode_images, desc=f"Producing single animal video for {os.path.basename(self._flyhostel_dataset)}. Chunk {chunk}"):
                     key_counter=0
                     with h5py.File(episode_image, "r") as file:
                         keys = list(file.keys())
                         while key_counter < len(keys):
-                            img=[]
+                            imgs=[]
                             for animal in range(self._number_of_animals):                            
                                 key=keys[key_counter]
                                 frame_number, blob_index = key.split("-")
@@ -127,29 +125,31 @@ class SingleVideoMaker:
                                     warnings.warn(f"More blobs than animals in frame_number {frame_number}")
 
                                 img_ = file[key][:]
-                                assert img.shape[0] <= height
-                                assert img.shape[1] <= width
-                                if self.video_writer is None: self.init_video_writer(frameSize=(width*self._number_of_animals, height))
+                                assert img_.shape[0] <= height
+                                assert img_.shape[1] <= width
+                                if self.video_writer is None: self.init_video_writer(basedir=basedir, frameSize=(width*self._number_of_animals, height))
                                 
                                 # angle=self.fetch_angle(frame_number, blob_index)
                                 # img=self.rotate_image(img, angle)
                                 
-                                img_=cv2.copyMakeBorder(img, 0, 0, max(0, width-img.shape[1]), max(0, height-img.shape[0]), cv2.BORDER_CONSTANT, self.background_color)
+                                img_=cv2.copyMakeBorder(img_, 0, max(0, height-img_.shape[0]), 0, max(0, width-img_.shape[1]), cv2.BORDER_CONSTANT, self.background_color)
                                 if img_.shape[0] > height:
                                     top = (img_.shape[0] // 2 - height // 2)
                                     img_=img_[top:(top+height), :]
                                 if img_.shape[1] > width:
                                     left = (img_.shape[1] // 2 - width // 2)
-                                    img_=img_[:, left:(left+width), :]
+                                    img_=img_[:, left:(left+width)]
 
-                                assert img_.shape[0] == height
-                                assert img_.shape[1] == width
+                                assert img_.shape[0] == height, f"{img_.shape[0]} != {height}"
+                                assert img_.shape[1] == width, f"{img_.shape[1]} != {width}"
+
 
                                 key_counter+=1
+                                imgs.append(img_)
 
-                            img = np.hstack(img.append(img_))
+                            img = np.hstack(imgs)
 
-                            cur.execute("SELECT frame_number FROM frames WHERE frame_number = ?", frame_number)
+                            cur.execute(f"SELECT frame_time FROM frames WHERE frame_number = {frame_number}")
                             frame_time = int(cur.fetchone()[0])
                             self.video_writer.add_image(img, frame_number, frame_time)
 
