c0b6f4e640b6febbe05a6ce5e72995b9d70f5cba
antortjim <ntoniohu@gmail.com>
2023-01-23 20:40:40 +0000

    Dont print

--- flyhostel/data/video.py
+++ flyhostel/data/video.py
@@ -137,12 +137,12 @@ class SingleVideoMaker:
 
             for chunk in chunks:
                 episode_images = sorted(glob.glob(os.path.join(basedir, "idtrackerai", f"session_{str(chunk).zfill(6)}", "segmentation_data", "episode_images*")), key=lambda f: int(os.path.splitext(f)[0].split("_")[-1]))
-                print(f"{len(episode_images)} hdf5 files found for chunk {chunk}")
+                # print(f"{len(episode_images)} hdf5 files found for chunk {chunk}")
                 for episode_image in tqdm(episode_images, desc=f"Producing single animal video for {os.path.basename(self._flyhostel_dataset)}. Chunk {chunk}"):
                     key_counter=0
                     with h5py.File(episode_image, "r") as file:
                         keys = list(file.keys())
-                        print(f"{len(keys)} keys found for chunk {chunk}")
+                        # print(f"{len(keys)} keys found for chunk {chunk}")
                         while key_counter < len(keys):
                             imgs=[]
                             for animal in range(self._number_of_animals):                            
