2abb14067f614b7a5707666a1786aa0161560930
antortjim <ntoniohu@gmail.com>
2023-02-12 23:42:35 +0100

    Change overwrite for reset

--- flyhostel/data/bin/export.py
+++ flyhostel/data/bin/export.py
@@ -8,7 +8,7 @@ def get_parser():
     ap.add_argument("--metadata", required=True, type=str, help="path to metadata.yaml")
     ap.add_argument("--tables", required=False, type=str, default="all", help="Tables to be written to sqlite file, all means all of them")
     ap.add_argument("--chunks", nargs="+", type=int, required=True, help="chunks to export")
-    ap.add_argument("--overwrite", action="store_true", default=False, help="If an existing dbfile is found, remove and make it from scratch")
+    ap.add_argument("--reset", action="store_true", default=False, help="If an existing dbfile is found, remove and make it from scratch")
     return ap
 
 
@@ -17,4 +17,4 @@ def main(args=None, ap=None):
         ap = get_parser(ap)
         args = ap.parse_args()
 
-    export_dataset(args.metadata, args.chunks, args.overwrite, tables=args.tables)
+    export_dataset(args.metadata, args.chunks, reset=args.reset, tables=args.tables)
