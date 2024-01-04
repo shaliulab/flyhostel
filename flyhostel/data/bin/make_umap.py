dc011b5afecfdb4a0ddac96be5a455a5e75ab7bc
antortjim <ntoniohu@gmail.com>
2024-01-04 20:16:40 +0000

    Clean make_umap and support reading wavelets in grouped dataset

--- flyhostel/data/bin/make_umap.py
+++ flyhostel/data/bin/make_umap.py
@@ -3,24 +3,17 @@ import logging
 import os.path
 import pickle
 import time
-
+from tqdm.auto import tqdm
 import hdf5storage
 import pandas as pd
 import numpy as np
 import plotly.express as px
 
 from umap import UMAP
-
-from flyhostel.data.pose.movie import connect_bps
-from flyhostel.data.bodyparts import bodyparts as BODYPARTS
-from flyhostel.data.pose.sleap import draw_video_row
-from flyhostel.data.pose.filters import filter_pose, arr2df
-from flyhostel.data.deg import read_label_file
 from flyhostel.data.pose.main import FlyHostelLoader
-from flyhostel.data.pose.fh_umap import add_n_steps_in_the_past
-lts_dir="/home/vibflysleep/opt/long_timescale_analysis/slurm/FlyHostel_long_timescale_analysis"
-wavelet_downsample=5
+from flyhostel.data.pose.constants import chunksize
 
+LTS_DATA=os.environ["LTS_DATA"]
 POSE_DATA=os.environ["POSE_DATA"]
 MOTIONMAPPER_DATA=os.environ["MOTIONMAPPER_DATA"]
 OUTPUT_FOLDER=os.path.join(MOTIONMAPPER_DATA, "output")
@@ -30,60 +23,59 @@ logger=logging.getLogger(__name__)
 
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-experiments=[
-    # "FlyHostel2_1X_2023-11-24_11-00-00",
-    # "FlyHostel1_1X_2023-11-24_11-00-00",
-    "FlyHostel1_1X_2023-09-28_16-00-00",
-    # "FlyHostel2_1X_2023-09-28_16-00-00",
-    # "FlyHostel1_1X_2023-11-08_17-00-00",
-    "FlyHostel1_1X_2023-11-03_13-00-00",
-    # "FlyHostel2_1X_2023-11-03_13-00-00",
-    # "FlyHostel1_1X_2023-11-13_11-00-00",
-    # "FlyHostel2_1X_2023-11-13_11-00-00",    
-]
-
-stride=75
-chunksize=45000
-framerate=10
-filters={"nanmedian": {"window_size": 0.2, "min_window_size": 40, "order": 0}, "nanmean": {"window_size": 0.2, "min_window_size":10, "order": 1}}
-interpolate_seconds={bp: 30 for bp in BODYPARTS}
-interpolate_seconds["proboscis"]=0.5
-ZT_START=14
-ZT_END=15
-min_score={bp: 0.5 for bp in BODYPARTS}
-min_score["proboscis"]=0.8
-bodyparts_xy=list(itertools.chain(*[[bp + "_x", bp + "_y"] for bp in BODYPARTS]))
-bodyparts_speed=list(itertools.chain(*[[bp + "_speed"] for bp in BODYPARTS]))
-bodyparts_speed
-min_time=ZT_START*3600
-max_time=ZT_END*3600
-
-
-dt0_features=["head_proboscis_distance"]
-feature_cols=bodyparts_speed + dt0_features
-feature_cols=dt0_features+bodyparts_xy
 NUMBER_OF_SAMPLES={"walk": 10_000, "inactive": 10_000, "groom": 10_000}
+stride=5
+RECOMPUTE=True
+
+
+def load_wavelets(loader):
+    """
+    Load pre-computed wavelet transform of pose of all flies in the loader
+    """
+    wavelets=[]
+    previous_freq_names=None
+    for i, datasetname in range(tqdm(loader.datasetnames)):
+
+        id=loader.identities[i]
+        matfile=os.path.join(LTS_DATA, "Wavelets", datasetname + "-pcaModes-wavelets.mat")
+
+        if not os.path.exists(matfile):
+            print(f"{matfile} not found")
+            return None    
+        data=hdf5storage.loadmat(matfile)
+        wavelets_single_animal=data["wavelets"]
+        freq_names=[f.decode() for f in data["freq_names"]]
+        if previous_freq_names is None:
+            previous_freq_names=freq_names
+        assert all([a == b for a, b in zip(freq_names, previous_freq_names)])
+        frequencies=data["f"]
+        wavelets_single_animal["id"]=id
+        wavelets_single_animal.set_index("id", append=True, inplace=True)
+        wavelets.append(wavelets_single_animal)
+    if len(loader.datasetnames)>1:
+        import ipdb;ipdb.set_trace()
+
+    wavelets=pd.concat(wavelets, axis=0)
+    return wavelets, (frequencies, freq_names)
+
 
 def load_dataset(loader):
 
     # load pose data and annotate it using ground truth
     #####################################################
-    pose=loader.pose_boxcar.copy()
+    loader.pose_speed_boxcar["id2"]=loader.pose_speed_boxcar["id"]
+    pose=loader.pose_speed_boxcar.set_index("id", append=True)
+    loader.pose_speed_boxcar.rename({"id2": "id"}, axis=1, inplace=True)
+
+    pose=loader.obtain_umap_input(pose, pose, dt0_features=["head_proboscis_distance"])
+
     pose_annotated=loader.annotate_pose(pose, loader.deg)
-    index=pose_annotated.index[::wavelet_downsample]
+    index=pose_annotated.index
 
     # load wavelet transform of the data
     #####################################################
-    matfile=os.path.join(lts_dir, "Wavelets", loader.datasetnames[0] + "-pcaModes-wavelets.mat")
+    wavelets, (frequencies, freq_names)=load_wavelets(loader)
 
-    if not os.path.exists(matfile):
-        print(f"{matfile} not found")
-        return None
-    
-    data=hdf5storage.loadmat(matfile)
-    wavelets=data["wavelets"]
-    freq_names=[f.decode() for f in data["freq_names"]]
-    frequencies=data["f"]
 
     # NOTE
     # For some reason, the wavelet transform may come with a number of rows
@@ -105,7 +97,6 @@ def load_dataset(loader):
     # generate a dataset of wavelets and the ground truth for all behaviors
     ##########################################################################
     pe_inactive=pose_annotated_with_wavelets.loc[pose_annotated_with_wavelets["behavior"]=="pe_inactive"]
-    n=pe_inactive.shape[0]
     behaviors=np.unique(pose_annotated_with_wavelets["behavior"]).tolist()
     for behav in ["unknown", "pe_inactive"]:
         if behav in behaviors:
@@ -129,44 +120,43 @@ def load_dataset(loader):
     labeled_dataset = pd.concat(dfs, axis=0)
     unknown_dataset = pose_annotated_with_wavelets.loc[pose_annotated_with_wavelets["behavior"]=="unknown"]
 
-    return labeled_dataset, unknown_dataset, freq_names
+    return labeled_dataset, unknown_dataset, (frequencies, freq_names)
 
 
 def main():
     loaders={}
 
+    assert os.path.exists("experiments.txt")
+    with open("experiments.txt", "r", encoding="utf-8") as handle:
+        lines=handle.readlines()
+        experiments=[line.strip("\n") for line in lines]
+
     for experiment in experiments:
-        print(experiment)
         loader = FlyHostelLoader(experiment, chunks=range(0, 400))
         loader.load_and_process_data(
-            min_time=min_time, max_time=max_time,
-            stride=stride, bodyparts=BODYPARTS,
+            stride=stride,
             cache="/flyhostel_data/cache",
-            filters=filters, min_score=min_score,
-            window_size_seconds=0.5, max_jump_mm=1,
-            interpolate_seconds=interpolate_seconds,
+            filters=None,
             useGPU=0
         )
         loaders[experiment]=loader
-    print("Done")
+
 
     datasets=[]
     unknown_datasets=[]
-    freq_names=None
 
     for experiment in experiments:
-        print(experiment)
         out=load_dataset(loaders[experiment])
         if out is None:
             continue
-        labeled_dataset, unknown_dataset, freq_names=out
+        labeled_dataset, unknown_dataset, (frequencies, freq_names)=out
+        assert freq_names is not None
+
         datasets.append(labeled_dataset)
         # generate a dataset of wavelets WITHOUT ground truth (represented by unknown behavior)
         unknown_datasets.append(unknown_dataset)
 
     del loaders
-    assert freq_names is not None
-
 
     unknown_dataset=pd.concat(unknown_datasets, axis=0)
     dataset=pd.concat(datasets, axis=0)
@@ -180,7 +170,6 @@ def main():
 
     # train the UMAP model
     # and use it to project the training and the test set
-    RECOMPUTE=True
     if RECOMPUTE:
         model=UMAP()
         umap_set=training_set[freq_names].values
@@ -195,8 +184,8 @@ def main():
         with open(UMAP_PATH, "rb") as handle:
             model=pickle.load(handle)
 
-    with open(os.path.join(OUTPUT_FOLDER, "freq_names.pkl"), "wb") as handle:
-        pickle.dump(freq_names, handle)
+    with open(os.path.join(OUTPUT_FOLDER, "freqs.pkl"), "wb") as handle:
+        pickle.dump((frequencies, freq_names), handle)
 
 
     logger.debug("Transforming labeled dataset of shape %s", training_set.shape)
