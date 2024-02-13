d084b52e5155caeeb1b4f746870a30a1dc42f509
antortjim <ntoniohu@gmail.com>
2024-02-13 18:42:04 +0000

    Update hostpy

--- flyhostel/data/hostpy.py
+++ flyhostel/data/hostpy.py
@@ -12,7 +12,7 @@ wavelet_downsample=setRunParameters().wavelet_downsample
 logger=logging.getLogger(__name__)
 
 
-def load_hostel(metadata, n_jobs=1):
+def load_hostel(metadata, n_jobs=1, value="dataframe"):
     """
     Load centroid and pose data for every fly provided in the metadata    
     """
@@ -24,62 +24,39 @@ def load_hostel(metadata, n_jobs=1):
         (metadata["number_of_animals"] != "NONE")
     ]
 
-    out=joblib.Parallel(n_jobs=n_jobs)(
-        joblib.delayed(
-            load_single_hostel_from_metadata
-        )(
-            *args
+    if value=="dataframe":
+        out=joblib.Parallel(n_jobs=n_jobs)(
+            joblib.delayed(
+                FlyHostelLoader.load_single_hostel_from_metadata
+            )(
+                *args
+            )
+            for args in zip(
+                metadata["flyhostel_number"],
+                metadata["number_of_animals"],
+                metadata["flyhostel_date"],
+                metadata["flyhostel_time"],
+                metadata["identity"]
+            )
         )
-        for args in zip(
-            metadata["flyhostel_number"],
-            metadata["number_of_animals"],
-            metadata["flyhostel_date"],
-            metadata["flyhostel_time"],
-            metadata["identity"]
-        )
-    )
-    data=[e[0] for e in out]
-    meta=[e[1] for e in out]
-    
-    data = pd.concat(data, axis=0)
-    meta = pd.concat(meta, axis=0)
-    data=to_behavpy(data=data, meta=meta)
-    return data
+        data=[d for d in out]
+        meta=[d.meta for d in out]
         
-
-def load_single_hostel_from_metadata(flyhostel_number, number_of_animals, flyhostel_date, flyhostel_time, identity):
-    experiment = f"FlyHostel{flyhostel_number}_{number_of_animals}X_{flyhostel_date}_{flyhostel_time}"
-    dt=load_single_hostel(experiment=experiment, identity=identity)
-    return dt
-
-
-def load_single_hostel(experiment, identity):
-
-    loader=FlyHostelLoader(experiment=experiment, identity=int(identity), chunks=range(0, 400))
-    loader.load_and_process_data(
-        stride=1,
-        cache="/flyhostel_data/cache",
-        filters=None,
-        useGPU=0,  
-    )
-
-    data=loader.dt.copy()
-    meta=data.meta.copy()
-    centroid_columns=data.columns.tolist()
-    data=data.loc[data["frame_number"] % wavelet_downsample == 0]
-
-    if loader.behavior is None:
-        logger.warning("Behavior not computed for %s", loader)
-        data["behavior"]=np.nan
-        data["score"]=np.nan
-        data["bout_in"]=np.nan
-        data["bout_out"]=np.nan
-        data["duration"]=np.nan
-    else:
-        data=data.merge(loader.behavior, on=["id", "frame_number"], how="left")
-
-
-    fields = centroid_columns + ["behavior",  "score", "bout_in", "bout_out", "duration"]
-    data=data[fields]
-    return data, meta
+        data = pd.concat(data, axis=0)
+        meta = pd.concat(meta, axis=0)
+        data=to_behavpy(data=data, meta=meta)
+        return data
+    
+    elif value=="object":
+        loaders={}
+        for _, row in metadata.iterrows():
+            loader=FlyHostelLoader.from_metadata(
+                row["flyhostel_number"],
+                row["number_of_animals"],
+                row["flyhostel_date"],
+                row["flyhostel_time"],
+                row["identity"]
+            )
+            loaders[loader.datasetnames[0]]=loader
+        return loaders
 
