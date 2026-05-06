0ce68012594b35e18386463b796c060e48e3f846
antortjim <ntoniohu@gmail.com>
2026-05-06 09:52:59 +0000

    Handle more than 4 polygon landmarks

--- flyhostel/data/sqlite3/landmarks.py
+++ flyhostel/data/sqlite3/landmarks.py
@@ -1,7 +1,16 @@
 import toml
 import os.path
 import sqlite3
+import logging
 from abc import ABC
+logger=logging.getLogger(__name__)
+
+def parse_landmark_line(line):
+    tokens=line.split(" ")
+    shape_name=tokens[1]
+    coords=" ".join(tokens[2:])
+
+    return shape_name, coords
 
 class LandmarksExporter(ABC):
     _basedir=None
@@ -21,14 +30,31 @@ class LandmarksExporter(ABC):
             raise FileNotFoundError(f"Please generate {landmarks_file}")
         
         with open(landmarks_file, "r") as handle:
-            roi_list=toml.load(handle)["roi_list"]
-            landmarks=[(line.split(" ")[1], " ".join(line.split(" ")[2:])) for line in roi_list]
+            config=toml.load(handle)
+
+        roi_list=config["roi_list"]
+        notch_count=config.get("notch_count", None)
+        if notch_count is None:
+            logger.warning("I will assume there are 4 notches")
+            notch_count=4
+
+        landmarks=[parse_landmark_line(line) for line in roi_list]
             
         landmark_types={"Ellipse": "food", "Polygon": "notch"}
-        landmarks=[(landmark_types[landmark[0]], landmark[1]) for landmark in landmarks]
+        landmarks2=[]
+        count=0
+        for shape_name, coords in landmarks:
+            landmark_name=landmark_types[shape_name]
+            if count == notch_count and shape_name == "Polygon":
+                landmark_name="Polygon"
+
+            if landmark_name=="notch":
+                count+=1
+
+            landmarks2.append((landmark_name, coords))
 
         with sqlite3.connect(dbfile, check_same_thread=False) as conn:
             conn.executemany(
                 "INSERT INTO LANDMARKS (shape, specification) VALUES (?, ?);",
-                landmarks
+                landmarks2
             )
