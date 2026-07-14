373ab339a90e67e0fe575e615dd3720b254bc581
antortjim <ntoniohu@gmail.com>
2026-07-14 08:58:26 +0000

    Use parallelizaiton in the proboscis extension pipeline

--- flyhostel/data/pose/proboscis_extension/pipeline.py
+++ flyhostel/data/pose/proboscis_extension/pipeline.py
@@ -5,18 +5,18 @@ from .extract_burst_traces import main as extract_burst_traces_for_fly
 from .make_burst_clips import main as make_burst_clips_for_fly
 
 
-def pipeline_for_fly(fly):
-    # proboscis_candidates_for_fly(fly)
-    # pe_features_for_fly(fly)
-    # extract_burst_traces_for_fly(fly, output = ".")
-    make_burst_clips_for_fly(fly, upscale=1, output=".")
-
+def pipeline_for_fly(fly, n_jobs):
+    proboscis_candidates_for_fly(fly)
+    pe_features_for_fly(fly)
+    extract_burst_traces_for_fly(fly, output = ".", n_jobs=n_jobs)
+    make_burst_clips_for_fly(fly, upscale=1, output=".", n_jobs=n_jobs)
+    
 
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("--fly", required=True)
     args=ap.parse_args()
-    pipeline_for_fly(args.fly)
+    pipeline_for_fly(args.fly, n_jobs=1)
 
 
 # ==========================================================================
