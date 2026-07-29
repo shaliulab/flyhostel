eb1a26414f8a0663afa25bc9be586b4221185a69
antortjim <ntoniohu@gmail.com>
2026-07-29 09:54:02 +0000

    Add docstring to derive_parameters

--- flyhostel/data/pose/proboscis_extension/derive_parameters.py
+++ flyhostel/data/pose/proboscis_extension/derive_parameters.py
@@ -1,3 +1,19 @@
+"""
+Derive the value of physical constraints of proboscis extension bouts
+using existing pose files listed in files.txt
+
+Parameters:
+    cone_rad: width of the angle in front of the head where the proboscis can be located, in radians
+    max_ext_mm: maximum head-proboscis distance allowed, in mm
+    max_tip_vel_mm_s: maximum velocity of the proboscis as it extends and protracts, in mm/s
+
+Example:
+{
+    "cone_rad": 0.697681725025177,
+    "max_ext_mm": 1.1090800762176514,
+    "max_tip_vel_mm_s": 19.673828125,
+}
+"""
 from .proboscis_candidates import load_and_derive_parameters
 
 # ==========================================================================
